8bf04538838638313a2fb4d1628d93ca90184583
Guillem Ferrer <guillemfd@gmail.com>
2022-04-05 00:38:04 +0200

    x4 katas

---
+++ Num_to_String.js
@@ -0,0 +1,9 @@
+//We need a function that can transform a number into a string.
+
+//123 --> "123"
+//999 --> "999"
+
+function numberToString(num) {
+    let numToString = num.toString();
+    return numToString
+  }
